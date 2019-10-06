74bf20741105b70171401477a65de890a18bcccf
DanielSmale <43572962+DanielSmale@users.noreply.github.com>
2019-10-06 14:56:14 +0100

    Began FPS, door and wrecking ball practices

---
+++ Physics practise/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed = 10.0f;
+    public float rotationSpeed = 5000f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey("w"))
+        {
+            rb.velocity = transform.forward * speed;
+        }
+
+        if (Input.GetKey("a"))
+        {
+            transform.Rotate(Vector3.up * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            transform.Rotate(Vector3.up * -speed * Time.deltaTime);
+
+        }
+
+        if (Input.GetKey("s"))
+        {
+            rb.velocity = -transform.forward * speed;
+        }
+    }
+
+}
---
+++ Physics practise/Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    public GameObject bullet;
+    public Rigidbody bulletRb;
+    public float bulletSpeed = 10000.0f;
+    public float damamge = 20f;
+
+
+    private void FixedUpdate()
+    {
+        Shoot();
+    }
+
+    private void Shoot()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(bullet, transform.position, transform.rotation);
+            bullet.GetComponent<Rigidbody>().velocity = transform.forward * bulletSpeed;
+
+        }
+        
+       
+    }
+
+}
