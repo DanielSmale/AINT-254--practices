9d101e29b6eb96a2bfaeebb1581ccc474774de01
DanielSmale <43572962+DanielSmale@users.noreply.github.com>
2019-10-31 12:36:42 +0000

    Made audio slider

---
+++ Sound practise/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField]
+    private AudioMixer audioMixer;
+
+
+    public void SetAmbientVolume(float _volume)
+    {
+        _volume = (int)(_volume * 100);
+
+
+        audioMixer.SetFloat("AmbientVolume", _volume);
+
+
+
+    }
+
+}
